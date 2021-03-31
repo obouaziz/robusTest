d0c66ee4d75151fedbe20c54b8861fa84fe629e2
obouaziz <olivier.bouaziz@parisdescartes.fr>
2021-04-01 00:24:06 +0200

    More tests have been added to the test function for tiebreak

--- tests/testthat/testtiebreak.R
+++ tests/testthat/testtiebreak.R
@@ -5,9 +5,19 @@ y <- c(4,9,12,11,2,10)
 xy_break=tiebreak(x,y)
 
 
-test_that("tiebreak works well",{
+test_that("tiebreak works well (on fixed samples)",{
   expect_equal(sum(duplicated(xbreak)),0)
   expect_equal(sum(xy_break$x%in%xy_break$y),0)
   expect_equal(length(tiebreak(x,y,nb_break=TRUE)),3)
   expect_equal(length(tiebreak(x,nb_break=TRUE)),2)
 })
+
+x<-sample(1:10,size=30,replace=TRUE)
+y<-sample(1:10,size=30,replace=TRUE)
+res_tab=table(x)
+
+test_that("tiebreak works well (on random samples)",{
+  expect_equal(tiebreak(x,nb_break=TRUE)$nb_break,sum(res_tab[unlist(res_tab)!=1]-1))
+  expect_equal(tiebreak(x,y,nb_break=TRUE)$nb_break,sum(x%in%y))
+})
+
